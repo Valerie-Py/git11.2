be613331ceceef6974d26beeb4f9bf54767721f3
ilita <ilitazdes@mail.ru>
2023-03-13 17:31:16 +0800

    add resolve conflicts

--- str_func.py
+++ str_func.py
@@ -3,6 +3,7 @@ def upper_str():
     user_input = input()
     return user_input.upper
 
+"""функция по переводу первых букв слов в верхний регистр"""
 def upcase_first_letter(s):
     s = "uiwerui"
     return s[0].upper() + s[1:]
