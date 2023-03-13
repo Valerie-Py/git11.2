c6b0dc8eb5d206587d429ad5ac62732692ed07e2
Apnoee <123247405+Valerie-Py@users.noreply.github.com>
2023-03-13 17:07:15 +0800

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -3,7 +3,5 @@ def upper_str():
     user_input = input()
     return user_input.upper
 
-
-"""функция по переводу первой буквы слова в заглавную"""
 def upcase_first_letter(s):
     return s[0].upper() + s[1:]
